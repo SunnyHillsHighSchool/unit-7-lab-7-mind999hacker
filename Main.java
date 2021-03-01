7a36bb66d9f0bee16aca09e55656f1a3cac56738
mind999hacker <75688231+mind999hacker@users.noreply.github.com>
2021-03-01 15:54:27 -0800

    Done

--- Main.java
+++ Main.java
@@ -25,5 +25,36 @@ public class Main
 				
 		//add more test cases
 
+		 wrds = "fun fly four six times ten plus eight".split(" ");
+		 test = new Words( wrds );
+		out.println(test);
+		out.println("word with 2 vowels = "+test.countWordsWithXVowels(2));
+		out.println("word with 3 vowels = "+test.countWordsWithXVowels(3));
+		out.println("word with 4 vowels = "+test.countWordsWithXVowels(4));
+		out.println("word with 2 chars = "+test.countWordsWithXChars(2));
+		out.println("word with 3 chars = "+test.countWordsWithXChars(3));		
+		out.println("word with 4 chars = "+test.countWordsWithXChars(4));
+		out.println("word with 5 chars = "+test.countWordsWithXChars(5));
+		 vowelsRemoved = test.removeWordsWithXChars(3);
+		out.println("\nafter removing words with 3 chars \n" + test);
+		out.println("\nnumber of vowels in the words removed == " + vowelsRemoved);
+		out.println("\n\n");
+
+
+		wrds = "alligator chicken dog cat pig buffalo".split(" ");
+		 test = new Words( wrds );
+		out.println(test);
+		out.println("word with 2 vowels = "+test.countWordsWithXVowels(2));
+		out.println("word with 3 vowels = "+test.countWordsWithXVowels(3));
+		out.println("word with 4 vowels = "+test.countWordsWithXVowels(4));
+		out.println("word with 2 chars = "+test.countWordsWithXChars(2));
+		out.println("word with 3 chars = "+test.countWordsWithXChars(3));		
+		out.println("word with 4 chars = "+test.countWordsWithXChars(4));
+		out.println("word with 5 chars = "+test.countWordsWithXChars(5));
+		out.println("word with 9 chars = "+test.countWordsWithXChars(9));
+		 vowelsRemoved = test.removeWordsWithXChars(3);
+		out.println("\nafter removing words with 3 chars \n" + test);
+		out.println("\nnumber of vowels in the words removed == " + vowelsRemoved);
+		out.println("\n\n");
 	}
 }
